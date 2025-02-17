9f3f184ed97cb9f4bbad7864ab5f243f19c21a13
김기태 <kimbeginner@ytstar.co.kr>
2025-02-17 09:02:11 +0900

    (*) 가상환경 관리 스크립트를 linux/window 공용으로 사용할 수 있도록 .py로 변경

---
+++ create_venv.py
@@ -0,0 +1,15 @@
+import os
+import platform
+import subprocess
+
+is_windows = platform.system() == "Windows"
+
+# 환경 변수 설정
+os.environ["PIPENV_VENV_IN_PROJECT"] = "true"
+os.environ["PIPENV_IGNORE_VIRTUALENVS"] = "1"
+
+# pipenv install 실행
+if platform.system() == "Windows":
+    subprocess.run(["pipenv", "install"], shell=True)
+else:
+    subprocess.run(["pipenv", "install"], shell=False)
--- create_venv.sh
+++
@@ -1,4 +0,0 @@
-export PIPENV_VENV_IN_PROJECT=1  # (macOS/Linux) 프로젝트 내부에 가상 환경 생성
-
-pip3 install  # 패키지 설치 및 가상 환경 생성
-pipenv shell  # 가상 환경 활성화
